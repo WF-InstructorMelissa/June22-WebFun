00623d77a4709c7eab720968c929abc44993f6a2
Apljc93 <apljc93@gmail.com>
2022-06-22 16:59:19 -0700

    Enter

---
+++ Personal Note/script.js
@@ -0,0 +1,27 @@
+// in-line comment
+/* multi-line comment */
+
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+//you're often going to set data into variable //basically a label to point to a data.
+
+var myName = "Beau"
+myName = 8
+
+/* 3 ways to declare variable: 
+1. var - used through the whole program
+2. let - only be used within the scope of where you declared that
+3. const -  variable that does not change and cannot change// will get an error if change is attemtped
+*/
+
+/* difference in assigning variables and declaring variables.
+End all lines in JavaScript with a semi-colon ";"
+
+e.g.,
+1. var a; - declaring a variable to be called a
+2. var b = 2; - declaring and assigning// declaring it "var b" and then the equals sign is the assignment operator.
+
+console.log() 
+
